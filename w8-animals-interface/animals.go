106ff60d906a5c2332bf2cec6433101251d1242a
Dmitrii Barsukov <dm.barsukov@gmail.com>
2020-05-29 18:33:05 +0700

    Week 8 - Animals with interface fixed (did not satisfy the task actually)

--- w8-animals-interface/animals.go
+++ w8-animals-interface/animals.go
@@ -12,7 +12,9 @@ type Animal interface {
 	Speak()
 }
 
-type Cow struct{}
+type Cow struct {
+	name string
+}
 
 func (a *Cow) Eat() {
 	fmt.Println("grass")
@@ -24,7 +26,9 @@ func (a *Cow) Speak() {
 	fmt.Println("moo")
 }
 
-type Bird struct{}
+type Bird struct {
+	name string
+}
 
 func (a *Bird) Eat() {
 	fmt.Println("worms")
@@ -36,7 +40,9 @@ func (a *Bird) Speak() {
 	fmt.Println("peep")
 }
 
-type Snake struct{}
+type Snake struct {
+	name string
+}
 
 func (a *Snake) Eat() {
 	fmt.Println("mice")
@@ -49,21 +55,24 @@ func (a *Snake) Speak() {
 }
 
 func main() {
-	fmt.Println("Type an animal name and an query space separated or word exit")
+	fmt.Println("Create a new animal: newanimal <Name> <Type>")
+	fmt.Println("Query an animal info: query <Name> <Action>")
 
-	var animalName, actionName string
+	var command, param1, param2 string
 	var animal Animal
+	animals := map[string]Animal{}
 	b := bufio.NewScanner(bufio.NewReader(os.Stdin))
 	for {
 		fmt.Print("> ")
 		b.Scan()
+		// exit
 		if b.Text() == "exit" {
 			os.Exit(0)
 		}
 
-		num, err := fmt.Sscan(b.Text(), &animalName, &actionName)
-		if num != 2 {
-			fmt.Println("Expected two words in the request")
+		num, err := fmt.Sscan(b.Text(), &command, &param1, &param2)
+		if num != 3 {
+			fmt.Println("Expected three words in the request")
 			continue
 		}
 		if err != nil {
@@ -71,30 +80,48 @@ func main() {
 			continue
 		}
 
-		switch animalName {
-		case "cow":
-			animal = &Cow{}
+		switch command {
+		case "newanimal":
+			if _, found := animals[param1]; found {
+				fmt.Printf("Animal with name %s already exists\n", param1)
+				continue
+			}
+			switch param2 {
+			case "cow":
+				animal = &Cow{param1}
+				break
+			case "bird":
+				animal = &Bird{param1}
+				break
+			case "snake":
+				animal = &Snake{param1}
+				break
+			default:
+				fmt.Printf("Not found an animal type %s\n", param2)
+				continue
+			}
+			animals[param1] = animal
 			break
-		case "bird":
-			animal = &Bird{}
-			break
-		case "snake":
-			animal = &Snake{}
+		case "query":
+			if _, found := animals[param1]; !found {
+				fmt.Printf("Not found %s animal\n", param1)
+				continue
+			}
+			animal = animals[param1]
+			switch param2 {
+			case "eat":
+				animal.Eat()
+			case "move":
+				animal.Move()
+			case "speak":
+				animal.Speak()
+			default:
+				fmt.Printf("Not found animal action %s\n", param2)
+				continue
+			}
 			break
 		default:
-			fmt.Printf("Not found an animal with a name: %s\n", animalName)
-			continue
-		}
-
-		switch actionName {
-		case "eat":
-			animal.Eat()
-		case "move":
-			animal.Move()
-		case "speak":
-			animal.Speak()
-		default:
-			fmt.Printf("No information how %s %s\n", animalName, actionName)
+			fmt.Printf("Command %s not supported\n", command)
 			continue
 		}
 	}
